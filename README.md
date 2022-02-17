b770e88ac628b0eb5ef27b99eed47c6036fda1ea
wangyunhua <10154231wang_yunhua@cn.tre-inc.com>
2022-02-17 14:06:29 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# test_github_release_pypi
+# douyin_image [![Version][version-badge]][version-link] ![MIT License][license-badge]
+
+
 test
+
+
+`print_text` 是一个个人测试,打印字符串
+
+
+### 示例
+
+
+### 使用方式
+
+```
+from print_text import add,remove,update
+print(add())
+print(remove())
+print(update())
+```
+
+
+### 安装
+
+```
+$ pip install print_text
+```
+
+
+### License
+
+[MIT](https://github.com/pythonml/douyin_image/blob/master/LICENSE)
+
+
+[version-badge]:   https://img.shields.io/badge/version-0.1-brightgreen.svg
+[version-link]:    https://pypi.python.org/pypi/douyin_image/
+[license-badge]:   https://img.shields.io/github/license/pythonml/douyin_image.svg
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="print_text",
+    version="0.1",
+    author="wu di",
+    author_email="",
+    description="test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ErraticO/test_github_release_pypi",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.14.4'],
+    entry_points={
+        'console_scripts': [
+            'add=print_text.add:make',
+            'remove=print_text.remove:make'
+            'update=print_text.update:make'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

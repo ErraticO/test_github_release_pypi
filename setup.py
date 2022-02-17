84cae20b4ca246d2d2453712b1e6ddc5a065db63
wangyunhua <10154231wang_yunhua@cn.tre-inc.com>
2022-02-17 15:41:20 +0800

    test1

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="print_text",
+    version="0.2",
+    author="wu di",
+    author_email="",
+    description="test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ErraticO/test_github_release_pypi",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts': [
+            'add=print_text.add:make',
+            'remove=print_text.remove:make'
+            'update=print_text.update:make'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
